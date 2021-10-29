45278160fdf18ee442289f988344c5df1ad80771
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-10-29 20:45:56 +0300

    refactored code / created: Section component / added styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,34 @@ import Section from './../Section/Section';
 const Home = () => {
   return (
     <div>
-      <Section />
+      <Section
+        title="Model S"
+        subtitle="Order Online for Touchless Delivery"
+        btnLeft="Custom Order"
+        btnRight="Existing Inventory"
+        background="model-s.jpg"
+      />
+      <Section
+        title="Model Y"
+        subtitle="Order Online for Touchless Delivery"
+        btnLeft="Custom Order"
+        btnRight="Existing Inventory"
+        background="model-y.jpg"
+      />
+      <Section
+        title="Model 3"
+        subtitle="Order Online for Touchless Delivery"
+        btnLeft="Custom Order"
+        btnRight="Existing Inventory"
+        background="model-3.jpg"
+      />
+      <Section
+        title="Model X"
+        subtitle="Order Online for Touchless Delivery"
+        btnLeft="Custom Order"
+        btnRight="Existing Inventory"
+        background="model-x.jpg"
+      />
     </div>
   );
 };
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import classes from './Section.module.css';
-import SectionText from './SectionText/SectionText';
-import SectionButtons from './SectionButtons/SectionButtons';
 
-function Section() {
+function Section({ title, subtitle, background, btnLeft, btnRight }) {
   return (
-    <div className={classes.wrap}>
-      <SectionText title="Model-X" subtitle="Order Online for Touchless Delivery" />
-      <SectionButtons />
+    <div className={classes.wrap} style={{ backgroundImage: `url('/images/${background}')` }}>
+      <div className={classes.textWrap}>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
+      <div>
+        <div className={classes.btnWrap}>
+          <button className={`${classes.btn} btn-reset`}>{btnLeft}</button>
+          <button className={`${classes.btn} btn-reset ${classes.rightBtn}`}>{btnRight}</button>
+        </div>
+        <img src="/images/down-arrow.svg" alt="Scroll Down" className={classes.arrowDown} />
+      </div>
     </div>
   );
 }
