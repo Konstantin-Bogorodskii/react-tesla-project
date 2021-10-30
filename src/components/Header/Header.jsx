8c63417bdda8043c1722ac20c33e7b4fcd0786a4
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-10-30 22:22:50 +0300

    created header component + menuburger / refactored code / connected dependencies

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
 import classes from './Header.module.css';
+import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Header = () => {
-  console.log(MenuIcon);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={classes.wrap}>
       <a href="#">
@@ -38,10 +42,73 @@ const Header = () => {
         <a href="#" className={classes.rightMenuLink}>
           Tesla Account
         </a>
-        <div className={classes.burger}></div>
+        <CustomMenu onClick={() => setIsOpen(true)} />
       </div>
+      <BurgerNav show={isOpen}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setIsOpen(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadaster</a>
+        </li>
+        <li>
+          <a href="#">Model S</a>
+        </li>
+        <li>
+          <a href="#">Model X</a>
+        </li>
+      </BurgerNav>
     </div>
   );
 };
 
 export default Header;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #fff;
+  width: 300px;
+  z-index: 100;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${props => (props.show ? 'translateX(0)' : 'translateX(100%)')};
+  transition: transform 0.3s ease-out;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
