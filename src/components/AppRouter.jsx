37d75ff32cd3d377513090f11a915a8444b76cd0
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-11-15 00:37:53 +0300

    fixed reload page bug / created Car component

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { publicRoutes, privateRoutes } from '../router/routes';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectUser } from './../store/reducers/userSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUser, login, logout } from './../store/reducers/userSlice';
+import { auth } from '../firebase-config';
 
 function AppRouter() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return user ? (
     <Switch>
---
+++ src/components/Car.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import ButtonSecondary from './../UI/ButtonSecondary';
+import styled from 'styled-components';
+
+function Car({ img, model, testDrive }) {
+  return (
+    <CarWrap>
+      <CarImg>
+        <img src={img} alt="" />
+      </CarImg>
+      <h2>{model}</h2>
+      <Actions>
+        <ButtonCar>order</ButtonCar>
+        {testDrive && <ButtonSecondary name="test drive" />}
+      </Actions>
+      <Info>
+        <span>Request a Call</span> to speak with a product specialist, value your trade-in or apply
+        for leasing
+      </Info>
+    </CarWrap>
+  );
+}
+
+const ButtonCar = styled.button`
+  background-color: #3e6ae1;
+  border: none;
+  padding: 11px 0;
+  border-radius: 50px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  margin-bottom: 30px;
+  transition: all 0.4s;
+  border: 3px solid #3e6ae1;
+
+  &:hover {
+    background-color: #3457b1;
+  }
+`;
+
+const CarWrap = styled.div`
+  display: grid;
+  place-items: center;
+  border-top: 1px solid #d0d1d2;
+
+  h2 {
+    font-weight: 500;
+    font-size: xx-large;
+    margin-top: -180px;
+    text-transform: capitalize;
+  }
+`;
+const CarImg = styled.div`
+  img {
+    object-fit: contain;
+    width: 1000px;
+  }
+`;
+const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-top: -40px;
+
+  button {
+    width: 230px;
+    margin-bottom: 30px;
+  }
+`;
+const Info = styled.p`
+  max-width: 350px;
+  text-align: center;
+  font-size: smaller;
+  line-height: 1.5;
+  font-weight: 500;
+  color: #393c41;
+
+  span {
+    color: #3e6be2;
+    cursor: pointer;
+  }
+`;
+
+export default Car;
