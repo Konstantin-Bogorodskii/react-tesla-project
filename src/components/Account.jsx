e8897c664fdfce00e161e56e796bef7e218d4785
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-11-15 00:36:52 +0300

    created account component / added signout function

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import Burger from './Burger';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useDispatch, useSelector } from 'react-redux';
+import { openBurger } from '../store/reducers/burgerSlice';
+import { logout, selectUser } from '../store/reducers/userSlice';
+import Car from './Car';
+import { auth } from '../firebase-config';
 
 function Account() {
-  const handlerLogout = () => {};
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const history = useHistory();
+
+  const handlerLogout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(logout());
+        history.push('/login');
+      })
+      .catch(err => alert(err.message));
+  };
 
   return (
     <Wrap>
@@ -33,53 +52,106 @@ function Account() {
             <StyledLink to="/account">solar panels</StyledLink>
           </li>
           <li>
-            <StyledLink onClick={handlerLogout}>log out</StyledLink>
+            <StyledLink to="" onClick={handlerLogout}>
+              log out
+            </StyledLink>
           </li>
+          <CustomMenu onClick={() => dispatch(openBurger())} />
+          <Burger />
         </MenuList>
       </Header>
+      <Info>
+        <Person>
+          <h4>{user?.displayName + ''}</h4>
+        </Person>
+        <InfoRight>
+          <Link to="/account">Home</Link>
+          <Link to="/account">Account</Link>
+          <Link to="/account">History</Link>
+          <Link to="" onClick={handlerLogout}>
+            Sign Out
+          </Link>
+        </InfoRight>
+      </Info>
+      <Cars>
+        <Car img="/images/tesla-s.jpg" model="model s" testDrive />
+        <Car img="/images/tesla-x.jpg" model="model x" />
+      </Cars>
     </Wrap>
   );
 }
 
 const Wrap = styled.div``;
-const Header = styled.div``;
-const Logo = styled.div``;
+const Header = styled.div`
+  padding: 14px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #222222;
+  color: white;
+`;
+const Logo = styled.div`
+  img {
+    object-fit: contain;
+    width: 80px;
+    filter: brightness(0) invert(1);
+  }
+`;
 
 const MenuList = styled.ul`
   display: flex;
+  gap: 20px;
   align-items: center;
-  justify-content: center;
-  gap: 15px;
 `;
 const StyledLink = styled(Link)`
-  display: inline-block;
-  font-weight: 600;
-  font-size: 15px;
+  color: white;
+  text-decoration: none;
+  font-weight: 500;
   text-transform: uppercase;
-  padding: 10px 10px 3px;
+  font-size: 15px;
+  z-index: 0;
+`;
+
+const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
-  position: relative;
+`;
 
-  &::before {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    width: 0;
-    height: 2px;
-    background-color: #232528;
-    opacity: 0.4;
-    transition: width 0.6s cubic-bezier(0.25, 1, 0.5, 1);
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 60px 140px;
+  padding-bottom: 30px;
+`;
+const Person = styled.div`
+  h4 {
+    font-weight: 700;
+    text-transform: capitalize;
+    font-size: x-large;
   }
+`;
+const InfoRight = styled.div`
+  display: flex;
+  gap: 20px;
 
-  &:hover {
-    &::before {
-      left: 0;
-      right: auto;
-      width: 100%;
+  a {
+    text-decoration: none;
+    color: #393c41;
+    font-size: smaller;
+    transition: all 0.2s;
+    text-transform: capitalize;
+
+    &:hover {
+      color: black;
+      font-weight: 500;
     }
   }
 `;
-// const Wrap = styled.div``;
+const Cars = styled.div`
+  display: grid;
+  place-items: center;
+  gap: 30px;
+  padding-bottom: 40px;
+`;
 
 export default Account;
